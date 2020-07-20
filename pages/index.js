8ffb42dfaac1369f930190af219e372e9bb375a7
Elyelin Carrasquero <elyelin15@gmail.com>
2020-07-20 17:27:58 -0300

    hello world

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default class extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>¡Hola Mundo!</h1>
+        <p>Desarrollando con Next.Js</p>
+      </div>
+    );
+  }
+}
